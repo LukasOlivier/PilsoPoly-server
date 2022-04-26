52dd63aa7411f7f27783659b311b73405dfc28d0
Student <sibren.eeckhout@student.howest.be>
2022-04-26 21:07:07 +0200

    addplayers ready to be used with the post call

---
+++ src/main/java/be/howest/ti/monopoly/logic/implementation/Move.java
@@ -0,0 +1,13 @@
+package be.howest.ti.monopoly.logic.implementation;
+
+public class Move {
+    private String tile;
+    private String description;
+    private String actionType;
+
+    public Move(String tile, String description, String actionType) {
+        this.tile = tile;
+        this.description = description;
+        this.actionType = actionType;
+    }
+}
--- src/main/java/be/howest/ti/monopoly/logic/implementation/turn.java
+++ src/main/java/be/howest/ti/monopoly/logic/implementation/turn.java
@@ -1,5 +1,11 @@
 package be.howest.ti.monopoly.logic.implementation;
 
-public class turn {
+import java.util.ArrayList;
+import java.util.List;
 
+public class turn {
+    private List<Integer> roll = new ArrayList<>();
+    private String player;
+    private String type;
+    private Move moves;
 }
