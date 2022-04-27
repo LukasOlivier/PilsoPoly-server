cd906d31d29397eb53cab1a02bb10cf9e8015dd7
Student <sibren.eeckhout@student.howest.be>
2022-04-27 21:41:29 +0200

    removed map

--- src/main/java/be/howest/ti/monopoly/logic/implementation/MonopolyService.java
+++ src/main/java/be/howest/ti/monopoly/logic/implementation/MonopolyService.java
@@ -1,15 +1,10 @@
 package be.howest.ti.monopoly.logic.implementation;
 import be.howest.ti.monopoly.logic.ServiceAdapter;
-import java.util.HashMap;
-import java.util.Map;
 import be.howest.ti.monopoly.logic.exceptions.MonopolyResourceNotFoundException;
 import java.util.List;
 
 
 public class MonopolyService extends ServiceAdapter {
-
-
-     Map globalList = new HashMap<String, Game>();
 
     @Override
     public String getVersion() {
