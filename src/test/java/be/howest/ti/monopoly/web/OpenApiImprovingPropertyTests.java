d7a3fa9b047699dfcc146e0ab2603b1328e7d8f5
Niels Soete <niels.soete@student.howest.be>
2022-05-16 10:03:01 +0200

    add server test

--- src/test/java/be/howest/ti/monopoly/web/OpenApiImprovingPropertyTests.java
+++ src/test/java/be/howest/ti/monopoly/web/OpenApiImprovingPropertyTests.java
@@ -97,6 +97,14 @@ class OpenApiImprovingPropertyTests extends OpenApiTestsBase {
 
     @Test
     void sellHotel(final VertxTestContext testContext) {
+
+        service.setDelegate( new ServiceAdapter() {
+
+            @Override
+            public void sellHotel(String gameId, String playerName, String propertyName) {}
+
+        });
+
         delete(
                 testContext,
                 "/games/game-id/players/Alice/properties/some-property/hotel",
