21b1d9232b8f873cfcdd590f1d33b7a2f67bd25e
Robin.Monseré <51537026+xdNiBoR@users.noreply.github.com>
2022-05-22 00:40:12 +0200

    Updated version

--- src/test/java/be/howest/ti/monopoly/logic/implementation/SpecificGameInfoTest.java
+++ src/test/java/be/howest/ti/monopoly/logic/implementation/SpecificGameInfoTest.java
@@ -3,7 +3,7 @@ package be.howest.ti.monopoly.logic.implementation;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+
 
 public class SpecificGameInfoTest {
     @Test
